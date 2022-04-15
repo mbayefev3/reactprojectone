fd062d9756ad05fcf089036ee0c4bbe1377de95b
mbaye <mbaye@february3@yahoo.com>
2022-04-14 23:38:15 -0700

    first commit

---
+++ src/components/Digits.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Digits = () => {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    return (
+        <div>
+            {
+
+            }
+        </div>
+    )
+}
+
+
+export default Digits
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Header = () => {
+
+    return (
+        <div>
+            <header>
+                <h1>Machine Calculator</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header
